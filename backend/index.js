74500a8b0b716e8325ab33439272022a47b2a447
anish <anishkr0098@gmail.com>
2026-02-14 11:06:56 +0530

    server created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
 
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const port = process.env.PORT;
+const app = express();
+
+app.get("/",(req,res) =>{
+    res.send("Hello From server");
+})
+
+app.listen(port,()=>{
+    console.log("server started");
+})
