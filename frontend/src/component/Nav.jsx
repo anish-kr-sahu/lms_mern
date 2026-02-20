7dc28a17835eaf9901b3567564116113b22f51a7
anish <anishkr0098@gmail.com>
2026-02-20 13:00:55 +0530

    profile created

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/logo.jpg"
 import { IoPersonCircle } from "react-icons/io5";
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,10 +8,16 @@ import {setUserData} from "../redux/userSlice"
 import axios from "axios"
 import { serverUrl } from '../App';
 import { toast } from 'react-toastify';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+
 function Nav() {
     const {userData} = useSelector(state=>state.user);
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [show , setShow ] = useState(false);
+    const [showHam, setShowHam ] = useState(false);
+    
 
     // handlelogin
     const handleLoginOut = async()=>{
@@ -31,14 +37,23 @@ function Nav() {
         <div className='lg:w-[20%] w-[40%] lg:pl-[50px]'>
             <img src={logo} alt="logo" className='w-[60px] rounded-[5px] border-2 border-white' />
         </div>
-        <div className='w-[30%] lg:flex items-center justify-center gap-4'>
-            {!userData && <IoPersonCircle className='w-[50px] h-[50px] fill-[black] cursor-pointer' />}
-            {userData && <div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer'>
+        <div className='w-[30%] lg:flex items-center justify-center gap-4 hidden'>
+            {!userData && <IoPersonCircle className='w-[50px] h-[50px] fill-[black] cursor-pointer' onClick={() => setShow(prev=>!prev)} />}
+            {userData && <div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer' onClick={() => setShow(prev=>!prev)}>
             {userData?.name.slice(0,1).toUpperCase()}
             </div>}
             {userData?.role === "educator" && <div className='px-[20px] py-[10px] border-2 border-white text-white bg-[black] rounded-[10px] text-[18px] font-light cursor-pointer'>Dashboard</div>}
             {!userData ?<span className='px-[20px] py-[10px] border-2 border-white text-white rounded-[10px] text-[18px] font-light cursor-pointer bg-[#000000d5]' onClick={() => navigate("/login")}>Login</span>:
             <span className='px-[20px] py-[10px] border-2 bg-white text-black rounded-[10px] text-[18px] shadow-sm cursor-pointer 'onClick={handleLoginOut}>Logout</span>}
+           {show &&<div className='absolute top-[110%] right-[15%] flex items-center flex-col jsutify-center gap-2 text-[16px] rounded-md bg-[white] px-[15px] py-[10px] border-[2px] border-black hover:border-white hover:text-white cursor-pointer hover:bg-black'>
+              <span className='bg-[black] text-white px-[30px] py-[10px] rounded-2xl hover:bg-gray-600'>My Profile</span>
+              <span className='bg-[black] text-white px-[30px] py-[10px] rounded-2xl hover:bg-gray-600'>My Courses</span>
+            </div>}
+        </div>
+        <RxHamburgerMenu className='w-[35px] h-[35px] lg:hidden text-black cursor-pointer' onClick={() => setShowHam(prev=>!prev)}/>
+        <div className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-[#000000d6] flex items-center justify-center flex-col gap-5 z-10 lg:hidden ${showHam ? "translate-x-[0] transition duration-600": "translate-x-[-100%] transition duration-600"}`}>
+         <RxCross2 className='w-[35px] h-[35px] fill-white absolute top-5 right-[4%]' onClick={()=> setShowHam(prev=> !prev)} />
+
         </div>
       </div>
     </div>
