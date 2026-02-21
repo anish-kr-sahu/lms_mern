419a5e9620d5a7e3c8e4203f6ccb632c822ad460
anish <anishkr0098@gmail.com>
2026-02-21 14:38:06 +0530

    forget-password created

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,32 +1,94 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import serverUrl from "../App"
+import { toast } from 'react-toastify';
+import { ClipLoader } from 'react-spinners';
 
 function ForgetPassword() {
-    const [step, setStep ] = useState(3);
+    const [step, setStep ] = useState(1); 
     const navigate = useNavigate();
+    const [email,setEmail] = useState("");
+    const [otp, setOtp] = useState("");
+    const [newPassword,setNewPassword] = useState("");
+    const [conPassword, setConPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+
+    // for step-1
+    const sendOTP = async () =>{
+        setLoading(true);
+        try{
+            const result = await axios.post(serverUrl + "/api/auth/sendotp", {email}, {withCredentials:true})
+            console.log(result.data);
+            setLoading(false);
+            setStep(2);
+            toast.success(result.data.msg);
+        }catch(error){
+            console.log(error);
+            toast.error(error.response.data.msg);
+            setLoading(false);
+        }
+    }
+
+    // step -2
+    const verifyOTP = async() =>{
+        setLoading(true);
+        try {
+            const result = await axios.post(serverUrl + "/api/auth/verifyotp",{email,otp}, {withCredentials:true});
+            console.log(result.data);
+            setLoading(false);
+            setStep(3);
+            toast.success(result.data.msg);
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.msg);
+            setLoading(false);
+        }
+    }
+
+    //step-3
+    const resetPassword = async() =>{
+        setLoading(true);
+        try {
+            if(newPassword !== conPassword){
+                return toast.error("password not matched");
+            }
+            const result = await axios.post(serverUrl + "/api/auth/resetpassword",{email,password:newPassword}, {withCredentials:true});
+            console.log(result.data);
+            setLoading(false);
+            navigate('/login');
+            toast.success(result.data.msg);
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.msg);
+            setLoading(false);
+        }
+    }
+
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4'>
       {/* step 1 */}
      { step == 1 && <div className='bg-white shadow-md rounded-xl p-8 max-w-md w-full'>
         <h2 className='text-2xl font-bold  mb-6 text-center text-gray-800'>Forget Your password</h2>
-        <form className='space-y-4'>
+        <form className='space-y-4' onSubmit={(e) =>e.preventDefault( )}>
             <div>
                 <label htmlFor="email" className='block text-sm font-medium text-gray-700'>Enter email</label>
-                <input id='email' type="email" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='you@example.com' required/>
+                <input id='email' type="email" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='you@example.com' onChange={(e) => setEmail(e.target.value)} value={email} required/>
             </div>
-            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer'>Send OTP</button>
+            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer' disabled={loading} onClick={sendOTP}>{loading? <ClipLoader size={30} color='white'/>: "Send OTP"}</button>
         </form>
         <div className='text-sm text-center mt-4 cursor-pointer' onClick={() =>navigate("/login")}>Back to login</div>
         </div>}
       {/* step 2 */}
      {step == 2 && <div className='bg-white shadow-md rounded-xl p-8 max-w-md w-full'>
         <h2 className='text-2xl font-bold  mb-6 text-center text-gray-800'>Enter OTP</h2>
-        <form className='space-y-4'>
+        <form className='space-y-4' onSubmit={(e) =>e.preventDefault()}>
             <div>
                 <label htmlFor="otp" className='block text-sm font-medium text-gray-700'>Please enter the 4-digit code sent to your email.</label>
-                <input id='otp' type="otp" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='****' required/>
+                <input id='otp' type="otp" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='* * * *' onChange={(e) => setOtp(e.target.value)} value={otp} required/>
             </div>
-            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer'>Verify OTP</button>
+            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer'disabled={loading} onClick={verifyOTP}>{loading? <ClipLoader size={30} color='white'/>: "Verify Otp"}</button>
         </form>
         <div className='text-sm text-center mt-4 cursor-pointer' onClick={() =>navigate("/login")}>Back to login</div>
         </div>}
@@ -35,17 +97,17 @@ function ForgetPassword() {
      { step == 3 &&  <div className='bg-white shadow-md rounded-xl p-8 max-w-md w-full'>
         <h2 className='text-2xl font-bold  mb-6 text-center text-gray-800'>Reset your Password</h2>
         <p className='text-sm text-gray-500 text-center mb-6'>Enter a new password below to regain access to your account.</p>
-        <form className='space-y-4'>
+        <form className='space-y-4' onSubmit={(e) => e.preventDefault()}>
             <div>
                 <label htmlFor="password" className='block text-sm font-medium text-gray-700'>New Password</label>
-                <input id='password' type="password" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='*********' required/>
+                <input id='password' type="password" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='*********' onChange={(e) => setNewPassword(e.target.value)} value={newPassword} required/>
             </div>
             <div>
-                <label htmlFor="confirm password" className='block text-sm font-medium text-gray-700'>Confirm Password</label>
-                <input id='conform password' type="password" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='*********' required/>
+                <label htmlFor="conPassword" className='block text-sm font-medium text-gray-700'>Confirm Password</label>
+                <input id='conPassword' type="password" className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black] 'placeholder='*********' onChange={(e) => setConPassword(e.target.value)} value={conPassword} required/>
             </div>
 
-            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer'>Reset Password</button>
+            <button className='w-full bg-[black] hover:bg-[#4b4b4b] text-white py-2 px-4 rounded-md font-medium cursor-pointer' disabled={loading} onClick={resetPassword}>{loading? <ClipLoader size={30} color='white'/>:"Reset Password"}</button>
         </form>
         <div className='text-sm text-center mt-4 cursor-pointer' onClick={() =>navigate("/login")}>Back to login</div>
         </div>}
