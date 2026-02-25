6ff8f939ce7ee656c742e86f7f15aec7b455d588
anish <anishkr0098@gmail.com>
2026-02-25 12:15:24 +0530

    editProfile created

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -39,7 +39,7 @@ function Nav() {
         </div>
         <div className='w-[30%] lg:flex items-center justify-center gap-4 hidden'>
             {!userData && <IoPersonCircle className='w-[50px] h-[50px] fill-[black] cursor-pointer' onClick={() => setShow(prev=>!prev)} />}
-            {userData && <div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer' onClick={() => setShow(prev=>!prev)}>
+            {userData?.photoUrl? <img src={userData?.photoUrl} className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer' onClick={() => setShow(prev=>!prev)}/>: <div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer' onClick={() => setShow(prev=>!prev)}>
             {userData?.name?.slice(0,1).toUpperCase()}
             </div>}
             {userData?.role === "educator" && <div className='px-[20px] py-[10px] border-2 border-white text-white bg-[black] rounded-[10px] text-[18px] font-light cursor-pointer'>Dashboard</div>}
@@ -50,11 +50,11 @@ function Nav() {
               <span className='bg-[black] text-white px-[30px] py-[10px] rounded-2xl hover:bg-gray-600'>My Courses</span>
             </div>}
         </div>
-        <RxHamburgerMenu className='w-[35px] h-[35px] lg:hidden text-black cursor-pointer' onClick={() => setShowHam(prev=>!prev)}/>
+        <RxHamburgerMenu className='w-[35px] h-[35px] lg:hidden text-white cursor-pointer' onClick={() => setShowHam(prev=>!prev)}/>
         <div className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-[#000000d6] flex items-center justify-center flex-col gap-5 z-10 lg:hidden ${showHam ? "translate-x-[0] transition duration-600": "translate-x-[-100%] transition duration-600"}`}>
          <RxCross2 className='w-[35px] h-[35px] fill-white absolute top-5 right-[4%]' onClick={()=> setShowHam(prev=> !prev)} />
             {!userData && <IoPersonCircle className='w-[50px] h-[50px] fill-[black] cursor-pointer' />}
-            {userData && <div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer'>
+            {userData?.photoUrl?<img src={userData?.photoUrl} className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer' onClick={() => setShow(prev=>!prev)}/> :<div className='w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-[black] border-white cursor-pointer'>
             {userData?.name?.slice(0,1).toUpperCase()}
             </div>}
              <div className='w-[200px] h-[65px] border-2 border-white text-white bg-[black] rounded-[10px] flex items-center justify-center text-[18px] font-light cursor-pointer' onClick={() => navigate("/profile")}>My Profile</div>
